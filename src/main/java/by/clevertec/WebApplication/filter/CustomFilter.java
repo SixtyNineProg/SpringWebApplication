8692c279cb1019c2182d6e69b39120914fdfdf84
Your NameDmitry <dima.klimov.98@inbox.ru>
2019-10-09 02:14:44 +0300

    multiran clasters

--- src/main/java/by/clevertec/WebApplication/filter/CustomFilter.java
+++ src/main/java/by/clevertec/WebApplication/filter/CustomFilter.java
@@ -4,28 +4,30 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
 @Slf4j
+@WebFilter("WebApplication/user")
 public class CustomFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        log.info("########## Initiating Custom filter ##########");
+        log.info("Initiating Custom filter");
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
-        System.out.println(httpRequest.getRequestURI());
+        log.info(httpRequest.getRequestURI());
+        log.info(String.valueOf(httpRequest.getLocalPort()));
         chain.doFilter(request, response);
     }
 
     @Override
     public void destroy() {
-
     }
 }
